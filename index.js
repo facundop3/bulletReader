e22f6fe10215e3f7de064701f46915bc324fe8dc
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2018-12-30 20:45:21 -0300

    Simple pdf text load (not ocr) from file. Added start pause and stop buttons

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const { app, BrowserWindow } = require('electron')
+"use strict";
+const { app, BrowserWindow, ipcMain } = require("electron"),
+      fs = require('fs'),
+      PDFParser = require("pdf2json"),
+      pdfParser = new PDFParser(this,1);
 
 function createWindow() {
-  const win = new BrowserWindow({ width: 800, height: 600, frame:false })
+  const win = new BrowserWindow({ width: 800, height: 600, frame:false });
 
-  win.loadFile('./src/views/index.html')
+  win.loadFile("./src/views/index.html");
+  win.openDevTools();
+
+  ipcMain.on('loadFile', (event, path) => {
+    console.log(path);
+    pdfParser.on("pdfParser_dataError", (errData) => {
+      console.error(errData.parserError);
+    });
+    pdfParser.on("pdfParser_dataReady", pdfData => {
+      event.sender.send('getFile',pdfParser.getRawTextContent().replace(/[\n\t]/g," ").split(" "));
+    });
+    pdfParser.loadPDF(path);
+  });
+
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,28 +1,81 @@
 (function(){
-
-  const $textContainer = document.getElementById("reader-text"),
+  const { ipcRenderer } = require('electron'),
+        $textContainer = document.getElementById("reader-text"),
         $speedInput = document.getElementById("speedRange"),
-        words = nextWord(text)
-  
+        $buttonStart = document.getElementById("actionStart"),
+        $buttonStop = document.getElementById("actionStop"),
+        $buttonLoadFile = document.getElementById("actionLoad"),
+        $loadFileInput = document.getElementById("loadFile");
+  let   playing = false,
+        words = null;
+
   let speed = $speedInput.value
-  
+
   $speedInput.addEventListener('change', function () {
     speed = this.value
   })
-  
 
-  bulletRead(words)
+  $buttonStart.addEventListener('click', function () {
+    const status = this.getAttribute("data-status");
+    switch(status){
+      case "start":
+        this.setAttribute("data-status","pause");
+        this.textContent="Pause";
+        playing=true;
+        $buttonStop.style.display="";
+        bulletRead(words);
+        break;
+      case "pause":
+        this.setAttribute("data-status","resume");
+        this.textContent="Resume";
+        playing=false;
+        break;
+      case "resume":
+        this.setAttribute("data-status","pause");
+        this.textContent="Pause";
+        playing=true;
+        bulletRead(words);
+        break;
+      default:
+        console.log(status);
+        break;
+    }
+  });
+  $loadFileInput.addEventListener("change",function () {
+    console.log(this.files[0].path);
+    ipcRenderer.send('loadFile', this.files[0].path)
+  });
+
+  $buttonLoadFile.addEventListener('click', function () {
+    $loadFileInput.click();
+  });
+
+  $buttonStop.addEventListener('click', function () {
+    $buttonStart.setAttribute("data-status","start");
+    $buttonStart.textContent="Start";
+    playing=false;
+    words=null;
+    this.style.display="none";
+  });
+
+  ipcRenderer.on('getFile', (event, arg) => {
+    words = nextWord(arg);
+    $buttonStart.style.display="";
+  })
+
 
   function* nextWord(wordsList){
     yield* wordsList
   }
   function bulletRead(words){
-    const {done, value} = words.next()
-    if(done) return
-    $textContainer.innerText = value 
-    setTimeout(()=>{
-      return bulletRead(words)
-    }, speed)
+    if(playing){
+      const {done, value} = words.next()
+      if(done) return
+      $textContainer.innerText = value
+      setTimeout(()=>{
+        return bulletRead(words)
+      }, speed)
+    }
   }
 
 })()
